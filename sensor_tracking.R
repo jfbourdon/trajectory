b6a421ed1f161f2ff1afdf7721acfa7b6330eecf
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2019-06-04 09:48:31 -0400

    Doc

--- sensor_tracking.R
+++ sensor_tracking.R
@@ -1,44 +1,43 @@
 #' Reconstruct the trajectory of the LiDAR sensor using multiple returns
 #'
 #' Use multiple returns to estimate the positionning of the sensor by computing the intersection in 
-#' space of the line passing througt the first and last returns. To work this function requieres a dataset
-#' where the 'gpstime', 'ReturnNumber', 'NumberOfReturns' and 'PointSourceID' attributes are properly 
-#' populated otherwise the output may be incorrect or weird.
+#' space of the line passing througt the first and last returns. To work this function requieres a 
+#' dataset where the 'gpstime', 'ReturnNumber', 'NumberOfReturns' and 'PointSourceID' attributes are 
+#' properly populated otherwise the output may be incorrect or weird.
 #' 
 #' When multiple returns from a single pulse are detected, the sensor compute their positions as being 
 #' in the center of the footprint and thus being all aligned. Because of that beheaviour, a line
 #' drawn between and beyond those returns must cross the sensor. Thus, several consecutive pulses
 #' emitted in a tight interval (e.g. 0.1 second) can be used to approximate an intersection
 #' point in the sky that correspond to the sensor position given that the sensor carrier hasn't
-#' moved much during this interval. A weighed least squares method using pseudoinverse gives an 
-#' approximation of the intersection by minimising the squared sum of the distances between the 
-#' intersection point and all the lines.
+#' moved much during this interval. A weighed least squares method gives an  approximation of the 
+#' intersection by minimising the squared sum of the distances between the intersection point and all 
+#' the lines.
 #' 
 #' @section Test of data integrity:
 #' In theory the sensor tracking is a simple problem to solve as long as each pulse is properly 
-#' identified from a well populated. In practice many problem may arise from wrongly populated datasets. 
-#' Here a list of problem that may happens. Those with a * denote already encountered problem and 
+#' identified from a well populated dataset. In practice many problems may arise from wrongly populated 
+#' datasets. Here a list of problem that may happens. Those with a * denote already encountered problem and 
 #' internally checked:
 #' \itemize{
 #' \item 'gpstime' do not record the time at with pulses were emitted and thus pulse are not idenfiable
 #' \item *A pulse (two or more points that share the same gpstime) is made of points from different 
 #' flightlines (different PointSourceID). This is impossible and denote wrongly populated PointSourceID
 #' attribute.
-#' }
-#' 
+#' }#' 
 #' 
 #' @template param-las
 #' @param interval numeric. Tight interval used to bin the gps times and group the pulses. 
-#' A sensor position in each interval.
 #' @param pmin interger. Minimum number of pulses needed to estimate a sensor position.
 #' For a given interval, the sensor position is not computed if the number of pulse is lower than 
 #' \code{pmin}.
 #' @param extra_check boolean. Dataset are rarely perfectly populated leading to unexpected errors. 
 #' Time consuming checks of data integrity are performed. These checks can be skipped as they account 
-#' for 75% of the computation time. See also section 'Tests of data integrity'
+#' for most of the computation time. See also section 'Tests of data integrity'
 #' 
-#' @return A SpatialPointDataFrame with 3D points (3 coordinates) and the information about the
-#' the time interval and the number of pulses used to find the points in the table of attributes.
+#' @return A SpatialPointDataFrame with the Z elevation stored in the table of attribute and the 
+#' information about the the time interval and the number of pulses used to find the points in the 
+#' table of attributes.
 #' 
 #' @author Jean-Francois Bourdon & Jean-Romain Roussel
 sensor_tracking <- function(las, interval = 0.5, pmin = 200, extra_check = TRUE)
@@ -98,7 +97,7 @@ sensor_tracking.LAS <- function(las, interval = 0.5, pmin = 200, extra_check = T
   # Find the position P of the sensor in each interval
   P <- data[, if (.N > 2*pmin) sensor_positions(X,Y,Z, ReturnNumber), by = .(gpstime = bins, PointSourceID = PointSourceID)]
   P <- P[!is.na(X)]
-  P <- sp::SpatialPointsDataFrame(P[,3:5], P[,c(1,2,6)])
+  P <- sp::SpatialPointsDataFrame(P[,3:4], P[,c(5,1,2,6)])
   
   return(P)
 }
@@ -149,7 +148,7 @@ sensor_positions <- function(x, y, z, rn)
   if (nrow(A) != nrow(B))
   {
     warning("Something went wrong. The point cloud is likely to be wrongly populated in a way not tested internally.", call. = FALSE)
-    return(list(X = NA_real_, Y = NA_real_, Z = NA_real_, N = NA_integer_))
+    return(list(X = NA_real_, Y = NA_real_, Z = NA_real_, npulses = NA_integer_))
   }
   
   # Lines described as vectors V
@@ -190,6 +189,3 @@ sensor_positions <- function(x, y, z, rn)
   
   return(list(X = M[1], Y = M[2], Z = M[3], npulses = nrow(A)))
 }
-
-# @param dmin numeric. Minimum distance between first and last returns. Multiple returns
-# with a distance shorter than \code{dmin} are not used to compute intersections.
